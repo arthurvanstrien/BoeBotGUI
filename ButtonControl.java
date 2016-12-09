4afa0ddeef1081f7cc3a44aacfd73a4ab62ca51d
ArthurvanStrien <arthurvanstrien@gmail.com>
2016-12-09 17:18:55 +0100

    Initial version of project commit. GUI elements added.

---
+++ ButtonControl.java
@@ -0,0 +1,95 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+/**
+ * Write a description of class ButtonControl here.
+ * 
+ * @author  (Arthur van Strien) 
+ * @version (1)
+ * @date    ()
+ */
+public class ButtonControl extends GUI
+{
+    public ButtonControl()
+    {
+        leftUpButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+               
+            }
+        });
+        
+        upButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+               
+            }
+        });
+        
+        rightUpButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+               
+            }
+        });
+        
+        leftButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+               
+            }
+        });
+        
+        stopButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+               
+            }
+        });
+        
+        rightButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+               
+            }
+        });
+        
+        leftDownButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+               
+            }
+        });
+        
+        downButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+               
+            }
+        });
+        
+        rightDownButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+               
+            }
+        });
+        
+        eightButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+               
+            }
+        });
+    }
+}
---
+++ GUI.java
@@ -0,0 +1,104 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+/**
+ * Write a description of class GUI here.
+ * 
+ * @author  (Arthur van Strien) 
+ * @version (1)
+ * @date    ()
+ */
+public class GUI extends JFrame
+{
+    JPanel panel;
+    JButton leftUpButton;
+    JButton upButton;
+    JButton rightUpButton;
+    JButton leftButton;
+    JButton stopButton;
+    JButton rightButton;
+    JButton leftDownButton;
+    JButton downButton;
+    JButton rightDownButton;
+    JButton eightButton;
+    
+    public GUI()
+    {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        panel = new JPanel(new BorderLayout());
+        setContentPane(panel);
+        createMenu();
+        
+        panel.add(createControlButtons(), BorderLayout.CENTER);
+        
+        setSize(500, 500);
+        setVisible(true);
+    }
+    
+    private void createMenu()
+    {
+        JMenuBar menubar = new JMenuBar();
+        setJMenuBar(menubar);
+            JMenu connection = new JMenu("Connectie");
+            menubar.add(connection);
+                JMenuItem openConnection = new JMenuItem("Open");
+                connection.add(openConnection);
+    }
+    
+    private JPanel createControlButtons()
+    {
+        JPanel buttons = new JPanel(new GridLayout(4, 3));
+        
+        ImageIcon leftUpImage = new ImageIcon("arrowupleft.png");
+        leftUpButton = new JButton(leftUpImage);
+        leftUpButton.setBackground(Color.WHITE);
+        buttons.add(leftUpButton);
+        
+        ImageIcon upImage = new ImageIcon("arrowup.png");
+        upButton = new JButton(upImage);
+        upButton.setBackground(Color.WHITE);
+        buttons.add(upButton);
+        
+        ImageIcon rightUpImage = new ImageIcon("arrowupright.png");
+        rightUpButton = new JButton(rightUpImage);
+        rightUpButton.setBackground(Color.WHITE);
+        buttons.add(rightUpButton);
+        
+        ImageIcon leftImage = new ImageIcon("arrowleft.png");
+        leftButton = new JButton(leftImage);
+        leftButton.setBackground(Color.WHITE);
+        buttons.add(leftButton);
+        
+        ImageIcon stopImage = new ImageIcon("stopSmall.jpg");
+        stopButton = new JButton(stopImage);
+        stopButton.setBackground(Color.WHITE);
+        buttons.add(stopButton);
+        
+        ImageIcon rightImage = new ImageIcon("arrowright.png");
+        rightButton = new JButton(rightImage);
+        rightButton.setBackground(Color.WHITE);
+        buttons.add(rightButton);
+        
+        ImageIcon leftDownImage = new ImageIcon("arrowdownleft.png");
+        leftDownButton = new JButton(leftDownImage);
+        leftDownButton.setBackground(Color.WHITE);
+        buttons.add(leftDownButton);
+        
+        ImageIcon downImage = new ImageIcon("arrowdown.png");
+        downButton = new JButton(downImage);
+        downButton.setBackground(Color.WHITE);
+        buttons.add(downButton);
+        
+        ImageIcon rightDownImage = new ImageIcon("arrowdownright.png");
+        rightDownButton = new JButton(rightDownImage);
+        rightDownButton.setBackground(Color.WHITE);
+        buttons.add(rightDownButton);
+        
+        ImageIcon eightImage = new ImageIcon("eight.png");
+        eightButton = new JButton(eightImage);
+        eightButton.setBackground(Color.WHITE);
+        buttons.add(eightButton);
+ 
+        return buttons;
+    }
+}
